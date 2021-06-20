6fdcb88f00269f6cc1723030ee569d65730fbbf2
Ninjasoturi <52629375+Ninjasoturi@users.noreply.github.com>
2021-06-20 20:49:12 +0300

    Everything doesn't need to be HTML

--- telegram/functions.php
+++ telegram/functions.php
@@ -548,7 +548,6 @@ function delete_message($chat_id, $message_id, $multicurl = false)
         'method'     => 'deleteMessage',
         'chat_id'    => $chat_id,
         'message_id' => $message_id,
-        'parse_mode' => 'HTML',
     ];
     if(!is_valid_target($chat_id, $message_id)){
       info_log("{$chat_id}/{$message_id}", 'ERROR: Cannot delete invalid chat/message id:');
@@ -580,7 +579,6 @@ function get_chat($chat_id, $multicurl = false)
     $reply_content = [
         'method'     => 'getChat',
         'chat_id'    => $chat_id,
-        'parse_mode' => 'HTML',
     ];
     if(!is_valid_target($chat_id, null, false, true)){
       info_log($chat_id, 'ERROR: Cannot get invalid chat id:');
@@ -612,7 +610,6 @@ function get_admins($chat_id, $multicurl = false)
     $reply_content = [
         'method'     => 'getChatAdministrators',
         'chat_id'    => $chat_id,
-        'parse_mode' => 'HTML',
     ];
     if(!is_valid_target($chat_id, null, false, true)){
       info_log($chat_id, 'ERROR: Cannot get invalid chat id:');
@@ -646,7 +643,6 @@ function get_chatmember($chat_id, $user_id, $multicurl = false)
         'method'     => 'getChatMember',
         'chat_id'    => $chat_id,
         'user_id'    => $user_id,
-        'parse_mode' => 'HTML',
     ];
     if(!is_valid_target($chat_id, null, false, true)){
       info_log($chat_id, 'ERROR: Cannot get invalid chat id:');
